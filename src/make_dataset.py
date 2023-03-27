260e42e4cd676f0d60ee733560696097c15d047e
Ilya Tampio <quakumei@gmail.com>
2023-03-27 19:35:54 +0300

    add: denoise start

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,5 +1,3 @@
-import logging
-
 def fetch_from_competition(competition_name:str, path:str):
     """
     Fetches data from a kaggle competition and saves it to a specified path
@@ -40,7 +38,31 @@ def make_cats_and_dogs(path:str, remove_residuals=True):
     if remove_residuals:
         clean_zips(path)
 
+def make_denoise(path:str):
+    """
+    https://www.kaggle.com/datasets/rajat95gupta/smartphone-image-denoising-dataset
+    """
+    COMPETITION_NAME = "rajat95gupta/smartphone-image-denoising-dataset"
+    fetch_from_competition(COMPETITION_NAME, path)
+
+    print("Unzipping data...")
+    unzip_data(f"{path}/smartphone-image-denoising-dataset.zip")
+
+AVAILABLE_DATASETS = {
+    "cats_and_dogs": make_cats_and_dogs,
+    "denoise": make_denoise,
+}
+
 if __name__ == "__main__":
-    make_cats_and_dogs('data/raw')
+    # Read in command line arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", help="The dataset to fetch", type=str)
+    parser.add_argument("--path", help="The path to save the data to", type=str, default="data/raw")
+    args = parser.parse_args()
 
-
+    # Fetch the dataset
+    if args.dataset in AVAILABLE_DATASETS:
+        AVAILABLE_DATASETS[args.dataset](args.path)
+    else:
+        raise Exception(f"Dataset {args.dataset} not available")
